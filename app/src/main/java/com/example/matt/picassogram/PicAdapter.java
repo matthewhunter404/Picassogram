4bfa2be607342c8cea83d7105722474d01bb9916
matt <matthewhunter404@gmail.com>
2016-07-13 14:03:57 +0200

    adds tons of images and basic picasso testing environment

--- app/src/main/java/com/example/matt/picassogram/PicAdapter.java
+++ app/src/main/java/com/example/matt/picassogram/PicAdapter.java
@@ -7,6 +7,8 @@ import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by matt on 2016/07/13.
  */
@@ -41,22 +43,85 @@ public class PicAdapter extends BaseAdapter {
             } else {
                 imageView = (ImageView) convertView;
             }
-
-            imageView.setImageResource(mThumbIds[position]);
+            Picasso.with(mContext)
+                    .load(mThumbIds[position])
+                    .resize(360, 360)
+                    .centerCrop()
+                    .placeholder(R.drawable.blurry) // optional
+                    .error(R.drawable.ic_placeholder)         // optional
+                    .into(imageView);
+            //imageView.setImageResource(mThumbIds[position]);
             return imageView;
         }
 
         // references to our images
         private Integer[] mThumbIds = {
-                R.drawable.pic1, R.drawable.pic3,
-                R.drawable.pic4, R.drawable.pic5,
-                R.drawable.pic6, R.drawable.pic7,
-                R.drawable.pic8, R.drawable.pic9,
-                R.drawable.pic10, R.drawable.pic11,
-                R.drawable.pic12, R.drawable.pic13,
-                R.drawable.pic2, R.drawable.pic14,
-                R.drawable.pic16, R.drawable.pic1,
-                R.drawable.pic15, R.drawable.pic17,
+                R.drawable.pic18s, R.drawable.pic3s,
+                R.drawable.pic4s, R.drawable.pic5s,
+                R.drawable.pic6s, R.drawable.pic7s,
+                R.drawable.pic8s, R.drawable.pic9s,
+                R.drawable.pic10s, R.drawable.pic11s,
+                R.drawable.pic12s, R.drawable.pic13s,
+                R.drawable.pic2s, R.drawable.pic14s,
+                R.drawable.pic16s, R.drawable.pic1s,
+                R.drawable.pic15s, R.drawable.pic17s,
+                R.drawable.pic18s, R.drawable.pic3s,
+                R.drawable.pic4s, R.drawable.pic5s,
+                R.drawable.pic6s, R.drawable.pic7s,
+                R.drawable.pic8s, R.drawable.pic9s,
+                R.drawable.pic10s, R.drawable.pic11s,
+                R.drawable.pic12s, R.drawable.pic13s,
+                R.drawable.pic2s, R.drawable.pic14s,
+                R.drawable.pic16s, R.drawable.pic1s,
+                R.drawable.pic15s, R.drawable.pic17s,
         };
+    private Integer[] mThumbId = {
+            R.drawable.j1, R.drawable.j9,
+            R.drawable.j2, R.drawable.j10,
+            R.drawable.j3, R.drawable.j11,
+            R.drawable.j4, R.drawable.j12,
+            R.drawable.j5, R.drawable.j14,
+            R.drawable.j6, R.drawable.j13,
+            R.drawable.j7, R.drawable.j1,
+            R.drawable.j8, R.drawable.j2,
+            R.drawable.j9, R.drawable.j3,
+            R.drawable.j10, R.drawable.j4,
+            R.drawable.j11, R.drawable.j5,
+            R.drawable.j12, R.drawable.j6,
+            R.drawable.j13, R.drawable.j7,
+            R.drawable.j14, R.drawable.j8,
+            R.drawable.j1, R.drawable.j9,
+            R.drawable.j3, R.drawable.j10,
+            R.drawable.j4, R.drawable.j11,
+            R.drawable.j5, R.drawable.j12,
+            R.drawable.j6, R.drawable.j13,
+            R.drawable.j7, R.drawable.j14,
+            R.drawable.j8, R.drawable.j1
+    };
+
+    private Integer[] mThumbIdh = {
+            R.drawable.h1, R.drawable.h9,
+            R.drawable.h2, R.drawable.h10,
+            R.drawable.h3, R.drawable.h11,
+            R.drawable.h4, R.drawable.h12,
+            R.drawable.h5, R.drawable.h7,
+            R.drawable.h6, R.drawable.h8,
+            R.drawable.h7, R.drawable.h1,
+            R.drawable.h8, R.drawable.h2,
+            R.drawable.h9, R.drawable.h3,
+            R.drawable.h10, R.drawable.h4,
+            R.drawable.h11, R.drawable.h5,
+            R.drawable.h12, R.drawable.h6,
+            R.drawable.h10, R.drawable.h7,
+            R.drawable.h11, R.drawable.h8,
+            R.drawable.h1, R.drawable.h9,
+            R.drawable.h3, R.drawable.h10,
+            R.drawable.h4, R.drawable.h11,
+            R.drawable.h5, R.drawable.h12,
+            R.drawable.h6, R.drawable.h2,
+            R.drawable.h7, R.drawable.h1,
+            R.drawable.h8, R.drawable.h11
+    };
+
     }
 
